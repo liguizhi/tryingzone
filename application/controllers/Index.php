f29207c0ffbae1527cc7c3da311abca8940e8a9e
liguizhi <liguizhi_001@163.com>
2014-05-26 20:21:26 +0800

    add ckeditor; modify index

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,7 +1,6 @@
 <?php
 class IndexController extends Yaf_Controller_Abstract {
     public function indexAction() {
-        var_dump("aaaa");
         $this->getView()->assign("content", "helloworld!");
     }
 }
