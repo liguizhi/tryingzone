11dbc2d7dd72ed982506ae754bbe6e41a7fc5f52
lgz <lgz@lgz-PC>
2014-08-25 21:14:17 +0800

    调整统计代码位置

--- wp-content/themes/earth-pro/footer.php
+++ wp-content/themes/earth-pro/footer.php
@@ -16,13 +16,13 @@
 				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'earthpro' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'earthpro' ), 'WordPress' ); ?></a>
 				<span class="sep"> | </span>
 				<?php printf( __( 'Theme: %1$s by %2$s.', 'earthpro' ), 'earthpro', '<a href="http://www.vathemes.com" rel="designer">VA Themes</a>' ); ?>
+                                <!--百度统计代码-->
+                                <script type="text/javascript">
+                                var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+                                document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Ff0ecf392a724dd69636fdf25c0cca708' type='text/javascript'%3E%3C/script%3E"));
+                                </script>
 			</div><!-- .site-info -->
 		<?php } else {earthpro_footer();} ?>
-        <!--百度统计代码-->
-        <script type="text/javascript">
-        var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-        document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Ff0ecf392a724dd69636fdf25c0cca708' type='text/javascript'%3E%3C/script%3E"));
-        </script>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
